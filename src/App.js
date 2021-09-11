dafb05280de9182f6c32eb37174eb3618eeef12c
user name <75030269+abhimstack@users.noreply.github.com>
2021-09-11 07:14:23 +0530

    index updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/header/Header.jsx';
 
 function App() {
   return (
     <div className='App'>
       <h4>Hello!</h4>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+  AppBar,
+  Typography,
+  Button,
+  Toolbar,
+  makeStyles,
+  alpha,
+  InputBase,
+  IconButton,
+} from '@material-ui/core';
+
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const menuId = 'primary-search-account-menu';
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography variant='h6' className={classes.title}>
+          FreshMart
+        </Typography>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder='Search…'
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+        <div className={classes.sectionDesktop}>
+          <IconButton
+            edge='end'
+            aria-label='account of current user'
+            aria-controls={menuId}
+            aria-haspopup='true'
+            onClick={handleProfileMenuOpen}
+            color='inherit'
+          >
+            <AccountCircle />
+          </IconButton>
+        </div>
+        <Button color='inherit'>Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
